c59691f60bbe0c554eb863d5e1478346650d8afa
RNN4830 <101938842+RNN4830@users.noreply.github.com>
2022-05-15 06:33:20 -0700

    Add files via upload

---
+++ age_year_check.py
@@ -0,0 +1,8 @@
+x = input("What is your name?")
+y = input("What is your age?")
+y = int(y)
+a = input("What age would you like to check?")
+a = int(a)
+z = 2022 + (a-y)
+print("Hi " + x + ". " "\nWhen you turn",a,"the year will be:",z)
+
