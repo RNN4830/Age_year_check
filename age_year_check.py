6c0cb9083ab1f9103490ea06c010cc49b8232674
RNN4830 <101938842+RNN4830@users.noreply.github.com>
2022-11-22 06:13:12 -0700

    Update age_year_check.py

--- age_year_check.py
+++ age_year_check.py
@@ -1,7 +1,7 @@
 x = input("What is your name?")
 y = input("What is your age?")
 y = int(y)
-a = input("What age would you like to check?")
+a = input("We can predict what year it will be when you turn a certain age.  What year would you like to check?")
 a = int(a)
 z = 2022 + (a-y)
 print("Hello " + x + ". " "\nWhen you turn",a,"the year will be:",z)
